364b3134b93c6ec78f45da19ac695cf98a1f9f1b
atn976 <119575129+atn976@users.noreply.github.com>
2024-02-20 09:39:53 +0100

    file of view model

---
+++ ViewModels/DiskSpaceViewModel.cs
@@ -0,0 +1,77 @@
+﻿using ratrapage_version_2._0.Models; // Importation de l'espace de noms pour accéder à d'autres classes du projet
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace ratrapage_version_2._0.ViewModels // Déclaration de l'espace de noms du ViewModel
+{
+    public class DiskSpaceViewModel : INotifyPropertyChanged // Définition de la classe DiskSpaceViewModel qui implémente l'interface INotifyPropertyChanged pour notifier les changements de propriété à la vue
+    {
+        private readonly DispatcherTimer _timer; // Utilisation d'un DispatcherTimer pour déclencher des événements à intervalles réguliers sur le thread de l'interface utilisateur
+        private int _updateInterval; // Stockage de l'intervalle de mise à jour de l'espace disque
+
+        // Propriété contenant les entrées de journal avec une notification de changement de propriété
+        public ObservableCollection<string> LogEntries { get; private set; }
+        public event PropertyChangedEventHandler? PropertyChanged; // Événement indiquant qu'une propriété a changé
+
+        // Propriété permettant de définir l'intervalle de mise à jour de l'espace disque avec notification de changement de propriété
+        public int UpdateInterval
+        {
+            get => _updateInterval;
+            set
+            {
+                if (_updateInterval != value)
+                {
+                    _updateInterval = value;
+                    OnPropertyChanged(nameof(UpdateInterval)); // Appel de la méthode OnPropertyChanged pour notifier la vue du changement de propriété
+                    UpdateTimer(); // Mise à jour du minuteur en conséquence
+                }
+            }
+        }
+
+        // Commande pour définir l'intervalle de mise à jour
+        public ICommand SetIntervalCommand { get; }
+
+        // Constructeur de la classe DiskSpaceViewModel
+        public DiskSpaceViewModel(int intervalInSeconds)
+        {
+            LogEntries = new ObservableCollection<string>(); // Initialisation de la collection des entrées de journal
+            _updateInterval = intervalInSeconds; // Initialisation de l'intervalle de mise à jour
+            _timer = new DispatcherTimer(); // Création d'une nouvelle instance de DispatcherTimer
+            _timer.Tick += (sender, e) => CheckDiskSpace(); // Définition de l'événement déclenché à chaque tic du minuteur
+            _timer.Interval = TimeSpan.FromSeconds(_updateInterval); // Définition de l'intervalle du minuteur
+            _timer.Start(); // Démarrage du minuteur pour déclencher les vérifications de l'espace disque
+
+            SetIntervalCommand = new RelayCommand(UpdateTimer); // Création de la commande pour définir l'intervalle de mise à jour
+        }
+
+        // Méthode pour mettre à jour le minuteur
+        private void UpdateTimer()
+        {
+            _timer.Stop(); // Arrêt du minuteur
+            _timer.Interval = TimeSpan.FromSeconds(_updateInterval); // Mise à jour de l'intervalle du minuteur
+            _timer.Start(); // Redémarrage du minuteur avec le nouvel intervalle
+        }
+
+        // Méthode pour vérifier l'espace disque disponible
+        private void CheckDiskSpace()
+        {
+            DriveInfo driveInfo = new DriveInfo("C"); // Création d'une instance DriveInfo pour le lecteur C
+            long availableSpace = driveInfo.AvailableFreeSpace; // Récupération de l'espace disque disponible
+            double availableSpaceGB = availableSpace / (1024.0 * 1024.0 * 1024.0); // Conversion de l'espace disponible en gigaoctets
+            string message = $"Available disk space on C: {availableSpaceGB:F2} GB ({availableSpace} bytes)"; // Création du message à afficher
+            LogManager.Instance.Log(message); // Enregistrement du message dans le journal via le gestionnaire de logs
+            Dispatcher.CurrentDispatcher.Invoke(() => { LogEntries.Add(message); }); // Ajout du message dans la collection des entrées de journal avec prise en compte de l'interface utilisateur
+            OnPropertyChanged(nameof(LogEntries)); // Notification de changement de propriété pour mettre à jour la vue avec les nouvelles entrées de journal
+        }
+
+        // Méthode pour notifier les changements de propriété
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); // Appel de l'événement PropertyChanged pour notifier les écouteurs de changements de propriété
+        }
+    }
+}
---
+++ ViewModels/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ratrapage_version_2._0.ViewModels
+{
+    // Utilisation du design pattern Command
+    // ICommand est une interface couramment utilisée pour encapsuler la logique d'une commande exécutable par l'interface utilisateur
+    // RelayCommand est une implémentation de ICommand qui permet de définir des actions exécutables par l'interface utilisateur
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute; // Action à exécuter lorsque la commande est appelée
+        private readonly Func<bool>? _canExecute; // Fonction déterminant si la commande peut être exécutée, nullable
+
+        // Événement qui est déclenché lorsque l'état de l'exécution de la commande peut être modifié
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; } // Ajoute un gestionnaire d'événements à l'événement RequerySuggested du CommandManager
+            remove { CommandManager.RequerySuggested -= value; } // Supprime un gestionnaire d'événements de l'événement RequerySuggested du CommandManager
+        }
+
+        // Constructeur de la classe RelayCommand
+        public RelayCommand(Action execute, Func<bool>? canExecute = null) // Le paramètre canExecute est nullable
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute)); // Vérifie que l'action à exécuter n'est pas nulle
+            _canExecute = canExecute; // Définit la fonction de vérification de l'exécution de la commande
+        }
+
+        // Méthode permettant de déterminer si la commande peut être exécutée
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke() ?? true; // Si _canExecute n'est pas nul, invoque la fonction de vérification, sinon retourne true
+        }
+
+        // Méthode qui effectue l'exécution de la commande
+        public void Execute(object? parameter)
+        {
+            _execute(); // Exécute l'action associée à la commande
+        }
+    }
+}
